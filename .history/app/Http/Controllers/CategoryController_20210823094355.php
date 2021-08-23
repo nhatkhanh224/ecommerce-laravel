b8aff2dcc96503c80b8c295922d6a049ec6747cf
nhatkhanh224 <55695197+nhatkhanhl@users.github.com>
2021-08-23 10:04:15 +0700

    First commit

---
+++ .history/app/Http/Controllers/CategoryController_20210823094355.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function create(){
+        return view("category.add");
+    }
+    public function create(){
+        return view("category.add");
+    }
+}
---
+++ .history/resources/views/category/add.blade_20210823100052.php
@@ -0,0 +1,58 @@
+@extends('layouts.admin')
+
+@section('title')
+<title>Add Category</title>
+@endsection
+
+@section('content')
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+<!-- Content Header (Page header) -->
+@include('partials.content-header', ['name' => 'Category', 'key' => 'add'])
+    <!-- /.content-header -->
+
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Nhập tên danh mục</label>
+              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                placeholder="Nhập tên danh mục">
+
+            </div>
+            <div class="form-group">
+              <label for="exampleFormControlSelect1">Chọn danh mục cha</label>
+              <select class="form-control" id="exampleFormControlSelect1">
+                <option>Chọn danh mục cha</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+  <!-- Control sidebar content goes here -->
+  <div class="p-3">
+    <h5>Title</h5>
+    <p>Sidebar content</p>
+  </div>
+</aside>
+@endsection
